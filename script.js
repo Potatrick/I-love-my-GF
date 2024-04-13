50389109839634fbd6c4626f1cb08d189868b40e
Patrick <patrickl.06013@gmail.com>
2024-04-13 01:39:23 -0700

    initialcommit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create an Audio object
+var audio = new Audio();
+
+// Set the source of the audio
+audio.src = 'audio/Ivy.mp3';
+
+// Function to play the audio
+function playAudio() {
+    // Play the audio
+    audio.play();
+}
