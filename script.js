0b4b80192855576dde86b68130496baf22d2f690
Patrick <patrickl.06013@gmail.com>
2024-05-14 11:37:36 -0700

    3rd MONTHHH
    Updated for my third month with the loml

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function playAudio() {
     // Play the audio
     audio.play();
 }
+
+// Create a second Audio object
+var audio2 = new Audio();
+
+// Set the source of the second audio
+audio2.src = 'audio/Congratulations.mp3';
+
+// Function to play the second audio
+function playSecondAudio() {
+    // Play the second audio
+    audio2.play();
+}
